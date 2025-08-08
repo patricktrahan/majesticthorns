ce92322602679f23e1386ff67c10b4f635dc91da
Patrick Trahan <34581132+patricktrahan@users.noreply.github.com>
2025-08-08 12:37:23 -0500

    Add files via upload
    website refresh

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Update the footer with the current year
+    document.getElementById("currentYear").textContent = new Date().getFullYear();
+});
